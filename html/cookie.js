3872d8de7b66fb3367475f3cce77c72f3c147f31
innovationlck <lck268@stern.nyu.edu>
2015-05-15 01:55:59 -0400

    adding bootstrap css to all the html files _ Added a few line breaks in the forms _ Also moved up cookie _js

---
+++ html/cookie.js
@@ -0,0 +1,13 @@
+function setCookie(key, value) {  
+   var expires = new Date();  
+   expires.setTime(expires.getTime() + 31536000000); //1 year  
+   document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();  
+   }  
+  
+function getCookie(key) {  
+   var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');  
+   return keyValue ? keyValue[2] : null;  
+   }  
+  function deleteCookie(key) {  
+    setCookie(key,"");
+   }
